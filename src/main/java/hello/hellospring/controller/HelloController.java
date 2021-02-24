32a8ade0a5eee15dc1e33d460b97eb9e4762c068
JM_CHOI_ <chlwjdals98@gmail.com>
2021-02-24 21:42:36 +0900

    API 생성하기

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //컨트롤러 어노테이션
 public class HelloController {
@@ -14,4 +16,41 @@ public class HelloController {
 
         return "hello"; //hello.html 값을 반환
     }
+
+    @GetMapping("hello-mvc")
+    //외부에서 name 이라는 이름의 파라미터 받아오기
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body의 문자 내용 직접 반환
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    //api 방식
+    //json 방식 key-value로 이루어진 구조
+    //옛날에는 xml도 썼는데 요즘은 json 쓰는 게 기본
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
